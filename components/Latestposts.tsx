4dde6360cdbd2abe8503de57ae9b5cfcf2b91310
poojitha <poojitha.1905l1@gmail.com>
2024-01-30 12:40:32 +0530

    latestposts

--- components/Latestposts.tsx
+++ components/Latestposts.tsx
@@ -1,36 +1,32 @@
-import React from 'react'
+import React from "react";
+import { data } from "@/data/data.json";
+import Link from "next/link";
 
 const Latestposts = () => {
+  const latestIds = ["6", "12", "18", "24", "30", "26", "42", "48", "54"];
+  const latestpostsdata = data.filter((item) => latestIds.includes(item.id));
+
   return (
-    <div className='w-1/6 min-h-9  rounded shadow-md flex flex-col justify-center items-center'>
-        <p className=' w-full py-4 px-4 text-xl font-bold bg-gradient-to-r from-rose-100 to-teal-100 rounded'>Latestposts</p>
-        <div className='flex flex-col justify-start items-start w-full py-2 px-4 border-b-1 border-black mx-1'>
-            <p className='font-semibold text-base'>WordPress</p>
-            <p>WordPress SEO Strategies</p>
-
-        </div>
-        <div className='flex flex-col justify-start items-start  w-full py-2 px-4 border-b-1 border-black'>
-            <p className='font-semibold text-base'>HTML</p>
-            <p>Web Accessibility with HTML</p>
-
-        </div>
-        <div className='flex flex-col justify-start items-start  w-full py-2 px-4 border-b-1 border-black'>
-            <p className='font-semibold text-base'>Java script</p>
-            <p>Async Programming in JavaScript</p>
-
-        </div>
-        <div className='flex flex-col justify-start items-start  w-full py-2 px-4 border-b-1 border-black'>
-            <p className='font-semibold text-base'>PHP</p>
-            <p>Secure Coding Practices in PHP</p>
-
-        </div>
-        <div className='flex flex-col justify-start items-start  w-full py-2 px-4 border-b-1 border-black'>
-            <p className='font-semibold text-base'>css</p>
-            <p>CSS Grid Layout Explained</p>
-
-        </div>
+    <div className="w-1/6 min-h-9  rounded shadow-md flex flex-col justify-center items-center">
+      <p className=" w-full py-4 px-4 text-xl font-bold bg-gradient-to-r from-rose-100 to-teal-100 rounded">
+        Latestposts
+      </p>
+      {latestpostsdata.map((item, index) => {
+        return (
+            <Link href="/[id]" as={`/${item.id}`}>
+            <div key={index} className="flex flex-col justify-start items-start w-full py-2 px-4 border-b-1 border-black mx-1">
+            <p className="font-semibold text-base">{item.techname}</p>
+            <p>{item.blogname}</p>
+          </div>
+            </Link>
+          
+        );
+      })}
+      {
+       
+      }
     </div>
-  )
-}
+  );
+};
 
-export default Latestposts
+export default Latestposts;
